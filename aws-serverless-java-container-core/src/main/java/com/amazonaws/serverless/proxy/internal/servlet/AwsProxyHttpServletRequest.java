9b189fd8e392349ffe495ae1590040414b9e0f35
Matt Corey <matt.corey@gmail.com>
2017-02-18 23:25:29 -0500

    Clean up getServerName() and getServerPort()

--- aws-serverless-java-container-core/src/main/java/com/amazonaws/serverless/proxy/internal/servlet/AwsProxyHttpServletRequest.java
+++ aws-serverless-java-container-core/src/main/java/com/amazonaws/serverless/proxy/internal/servlet/AwsProxyHttpServletRequest.java
@@ -268,7 +268,7 @@ public class AwsProxyHttpServletRequest
     @Override
     public StringBuffer getRequestURL() {
         String url = "";
-        url += getHeaderCaseInsensitive(HttpHeaders.HOST);
+        url += getServerName();
         url += "/";
         url += getContextPath();
         url += "/";
@@ -613,13 +613,18 @@ public class AwsProxyHttpServletRequest
 
     @Override
     public String getServerName() {
-        return "lambda.amazonaws.com";
+        String name = getHeaderCaseInsensitive(HttpHeaders.HOST);
+
+        if (name == null || name.length() == 0) {
+            name = "lambda.amazonaws.com";
+        }
+        return name;
     }
 
 
     @Override
     public int getServerPort() {
-        return 0;
+        return getLocalPort();
     }
 
 
@@ -725,7 +730,14 @@ public class AwsProxyHttpServletRequest
 
     @Override
     public int getLocalPort() {
-        return 0;
+        int port = 0;
+
+        if ("https".equals(getScheme())) {
+            port = 443;
+        } else if ("http".equals(getScheme())) {
+            port = 80;
+        }
+        return port;
     }
 
 
