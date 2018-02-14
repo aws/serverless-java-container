f40315e7a427e42cc1c87b1f762891ee658eb722
sapessi <stefano.buliani@gmail.com>
2018-02-14 08:19:36 -0800

    Added dependency check plugin for the library and updated dependencies in samples.

--- aws-serverless-java-container-core/src/main/java/com/amazonaws/serverless/proxy/internal/testutils/MockLambdaConsoleLogger.java
+++ aws-serverless-java-container-core/src/main/java/com/amazonaws/serverless/proxy/internal/testutils/MockLambdaConsoleLogger.java
@@ -14,6 +14,9 @@ package com.amazonaws.serverless.proxy.internal.testutils;
 
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 
+import java.nio.charset.Charset;
+
+
 /**
  * Mock LambdaLogger object that prints output to the console
  */
@@ -26,5 +29,11 @@ public class MockLambdaConsoleLogger implements LambdaLogger {
     @Override
     public void log(String s) {
         System.out.println(s);
+    }
+
+
+    @Override
+    public void log(byte[] bytes) {
+        System.out.println(new String(bytes, Charset.defaultCharset()));
     }
 }
