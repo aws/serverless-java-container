9bf5e7a03decdab767369b1011c76bcc031a5393
sapessi <stefano.buliani@gmail.com>
2018-12-19 22:27:25 -0800

    Removed unused imports

--- aws-serverless-java-container-spring/src/test/java/com/amazonaws/serverless/proxy/spring/springbootapp/TestApplication.java
+++ aws-serverless-java-container-spring/src/test/java/com/amazonaws/serverless/proxy/spring/springbootapp/TestApplication.java
@@ -2,15 +2,11 @@ package com.amazonaws.serverless.proxy.spring.springbootapp;
 
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.filter.CharacterEncodingFilter;
 
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.amazonaws.serverless.proxy.spring.springbootapp")
 public class TestApplication extends SpringBootServletInitializer {
-    
 }
