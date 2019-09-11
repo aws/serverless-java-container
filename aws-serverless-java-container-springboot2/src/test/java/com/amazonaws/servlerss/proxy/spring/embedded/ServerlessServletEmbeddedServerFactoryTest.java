248fad3f84bbf5f8ba3538757bda53b02076e183
sapessi <stefano.buliani@gmail.com>
2019-09-11 13:56:04 -0700

    Unit test for Servlet embedded factory

---
+++ aws-serverless-java-container-springboot2/src/test/java/com/amazonaws/servlerss/proxy/spring/embedded/ServerlessServletEmbeddedServerFactoryTest.java
@@ -0,0 +1,46 @@
+package com.amazonaws.servlerss.proxy.spring.embedded;
+
+import com.amazonaws.serverless.exceptions.ContainerInitializationException;
+import com.amazonaws.serverless.proxy.AwsProxyExceptionHandler;
+import com.amazonaws.serverless.proxy.AwsProxySecurityContextWriter;
+import com.amazonaws.serverless.proxy.internal.servlet.AwsProxyHttpServletRequestReader;
+import com.amazonaws.serverless.proxy.internal.servlet.AwsProxyHttpServletResponseWriter;
+import com.amazonaws.serverless.proxy.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import com.amazonaws.serverless.proxy.spring.embedded.ServerlessServletEmbeddedServerFactory;
+import org.junit.Test;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+import static org.junit.Assert.fail;
+
+public class ServerlessServletEmbeddedServerFactoryTest {
+    private SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler = new SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse>(
+            AwsProxyRequest.class,
+            AwsProxyResponse.class,
+            new AwsProxyHttpServletRequestReader(),
+            new AwsProxyHttpServletResponseWriter(),
+            new AwsProxySecurityContextWriter(),
+            new AwsProxyExceptionHandler(),
+            null
+    );
+
+    public ServerlessServletEmbeddedServerFactoryTest() throws ContainerInitializationException {
+    }
+
+    @Test
+    public void getWebServer_callsInitializers() {
+        ServerlessServletEmbeddedServerFactory factory = new ServerlessServletEmbeddedServerFactory();
+        factory.getWebServer(new ServletContextInitializer() {
+            @Override
+            public void onStartup(ServletContext servletContext) throws ServletException {
+                if (servletContext == null) {
+                    fail("Null servlet context");
+                }
+            }
+        });
+    }
+}
