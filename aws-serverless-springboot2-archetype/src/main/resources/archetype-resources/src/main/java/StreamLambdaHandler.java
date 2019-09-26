1d3456763f32fda30c065637d3c3f2517f32a16b
sapessi <stefano.buliani@gmail.com>
2019-09-26 12:44:07 -0700

    Added comment explaining async initializer in springboot 2 archetyoe

--- aws-serverless-springboot2-archetype/src/main/resources/archetype-resources/src/main/java/StreamLambdaHandler.java
+++ aws-serverless-springboot2-archetype/src/main/resources/archetype-resources/src/main/java/StreamLambdaHandler.java
@@ -18,6 +18,13 @@ public class StreamLambdaHandler implements RequestStreamHandler {
     static {
         try {
             handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class);
+            // For applications that take longer than 10 seconds to start, use the async builder:
+            // long startTime = Instant.now().toEpochMilli();
+            // handler = new SpringBootProxyHandlerBuilder()
+            //                    .defaultProxy()
+            //                    .asyncInit(startTime)
+            //                    .springBootApplication(Application.class)
+            //                    .buildAndInitialize();
         } catch (ContainerInitializationException e) {
             // if we fail here. We re-throw the exception to force another cold start
             e.printStackTrace();
