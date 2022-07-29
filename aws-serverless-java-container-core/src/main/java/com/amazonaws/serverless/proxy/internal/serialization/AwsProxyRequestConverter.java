1cbbee92a8ac3be6098ebde82c0038df6791891e
Andy Boothe <andy.boothe@gmail.com>
2022-07-28 23:41:49 -0500

    reduce method complexity per CR

--- aws-serverless-java-container-core/src/main/java/com/amazonaws/serverless/proxy/internal/serialization/AwsProxyRequestConverter.java
+++ aws-serverless-java-container-core/src/main/java/com/amazonaws/serverless/proxy/internal/serialization/AwsProxyRequestConverter.java
@@ -17,38 +17,46 @@ import com.fasterxml.jackson.databind.util.Converter;
 public class AwsProxyRequestConverter implements Converter < AwsProxyRequest, AwsProxyRequest > {
   @Override
   public AwsProxyRequest convert(AwsProxyRequest value) {
-      if (value == null)
-          return null;
+        if (value == null)
+            return null;
 
-      if ((value.getMultiValueHeaders() == null || value.getMultiValueHeaders().isEmpty()) &&
-          value.getHeaders() != null && !value.getHeaders().isEmpty()) {
-          Headers multiValueHeaders = new Headers();
-          for (Map.Entry < String, String > e: value.getHeaders().entrySet())
-              multiValueHeaders.putSingle(e.getKey(), e.getValue());
-          value.setMultiValueHeaders(multiValueHeaders);
-      }
+        if ((value.getMultiValueHeaders() == null || value.getMultiValueHeaders().isEmpty()) &&
+                value.getHeaders() != null && !value.getHeaders().isEmpty()) {
+            copySingleValueHeadersToMultiValueHeaders(value);
+        }
 
-      if ((value.getMultiValueQueryStringParameters() == null ||
-              value.getMultiValueQueryStringParameters().isEmpty()) &&
-          value.getQueryStringParameters() != null &&
-          !value.getQueryStringParameters().isEmpty()) {
-          MultiValuedTreeMap < String, String > multiValueQueryStringParameters =
-              new MultiValuedTreeMap < > ();
-          for (Map.Entry < String, String > e: value.getQueryStringParameters().entrySet())
-              multiValueQueryStringParameters.putSingle(e.getKey(), e.getValue());
-          value.setMultiValueQueryStringParameters(multiValueQueryStringParameters);
-      }
+        if ((value.getMultiValueQueryStringParameters() == null ||
+                value.getMultiValueQueryStringParameters().isEmpty()) &&
+                value.getQueryStringParameters() != null &&
+                !value.getQueryStringParameters().isEmpty()) {
+            copySingleValueQueryStringParametersToMultiValueQueryStringParameters(value);
+        }
 
-      return value;
-  }
+        return value;
+    }
 
-  @Override
-  public JavaType getInputType(TypeFactory typeFactory) {
-      return typeFactory.constructType(AwsProxyRequest.class);
-  }
+    private void copySingleValueHeadersToMultiValueHeaders(AwsProxyRequest value) {
+        Headers multiValueHeaders = new Headers();
+        for (Map.Entry < String, String > e: value.getHeaders().entrySet())
+            multiValueHeaders.putSingle(e.getKey(), e.getValue());
+        value.setMultiValueHeaders(multiValueHeaders);
+    }
 
-  @Override
-  public JavaType getOutputType(TypeFactory typeFactory) {
-      return typeFactory.constructType(AwsProxyRequest.class);
-  }
+    private void copySingleValueQueryStringParametersToMultiValueQueryStringParameters(AwsProxyRequest value) {
+        MultiValuedTreeMap < String, String > multiValueQueryStringParameters =
+            new MultiValuedTreeMap < > ();
+        for (Map.Entry < String, String > e: value.getQueryStringParameters().entrySet())
+            multiValueQueryStringParameters.putSingle(e.getKey(), e.getValue());
+        value.setMultiValueQueryStringParameters(multiValueQueryStringParameters);
+    }
+
+    @Override
+    public JavaType getInputType(TypeFactory typeFactory) {
+        return typeFactory.constructType(AwsProxyRequest.class);
+    }
+
+    @Override
+    public JavaType getOutputType(TypeFactory typeFactory) {
+        return typeFactory.constructType(AwsProxyRequest.class);
+    }
 }
